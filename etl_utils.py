86cfece96473c68a03244ff76b8f2deffb639a1f
Kimaru thagana <macbuntu@localhost.localdomain>
2021-05-31 13:19:14 +0300

    explain the etl tasks

--- etl_utils.py
+++ etl_utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import requests
+import numpy as np
 
 '''
 The faux data lake is to represent a cloud based storage like s3 or GCS
@@ -35,6 +36,10 @@ def get_basket_analysis_dataset():
     group by purchase ID and store data
     '''
     transaction_df = pd.read_csv('faux_data_lake/transaction_df.csv')
+    transaction_df = transaction_df[['product_id','quantity','purchase_id']]
+    # pivot to have purchase ID as index, product IDs as columns and quantity(mean) as the values
+    # for basket analysis, one is considering what goes together(cause and effect). So average is a better agg function
+    transaction_df.pivot(index='purchase_id', columns='product_id',values='quantity')
     transaction_df.to_csv('faux_data_lake/basket_analysis.csv')
 
 
@@ -43,7 +48,15 @@ def get_recommendation_engine_dataset():
     group by user ID and send to data lake as dataset
     '''
     transaction_df = pd.read_csv('faux_data_lake/transaction_df.csv')
+    transaction_df = transaction_df[['user_id','quantity','product_id']]
+    # pivot to have user ID as index, product IDs as columns and quantity(sum) as the values
+    # for recommendation engines, it may be critical to know what kind of products are bought in large quantities over time
+    transaction_df = pd.pivot_table(transaction_df,index='user_id', 
+                   columns='product_id',
+                   values='quantity', aggfunc=np.sum)
+    
     transaction_df.to_csv('faux_data_lake/recommendation_engine_analysis.csv')
+    
 
 # modeled as loading job
 
