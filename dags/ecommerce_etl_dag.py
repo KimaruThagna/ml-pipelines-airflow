fb5f67bc33f49738fd5d3a124815f620a6083b79
Kimaru thagana <macbuntu@localhost.localdomain>
2021-05-31 15:12:22 +0300

    build main task group

--- dags/ecommerce_etl_dag.py
+++ dags/ecommerce_etl_dag.py
@@ -1,19 +1,37 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
+from airflow.operators.python PythonOperator
 from airflow.utils.task_group import TaskGroup
 
 from datetime import datetime, timedelta
+
+from ..etl_utils import *
+
 default_args = {
     "owner": "airflow",
     "depends_on_past": False,
     "start_date": datetime(2020, 8, 13),
-    "email": ["airflow@airflow.com"],
+    "email": ["kimaru@thagana.com"],
     "email_on_failure": False,
     "email_on_retry": False,
-    "retries": 1,
+    "retries": 3,
     "retry_delay": timedelta(minutes=5),
     "catchup": False,
 }
 
 with DAG("ECOMMERCE_ETL_DAG", default_args=default_args, schedule_interval=timedelta(days=1)) as dag: 
-    
+    with TaskGroup("Extraction_Tasks") as extraction_group: 
+        extract_users_task = PythonOperator(
+            task_id='extract_users_task',
+            python_callable=pull_user_data
+        )
+        
+        extract_products_task = PythonOperator(
+            task_id='extract_products_task',
+            python_callable=pull_product_data
+        )
+         
+        extract_teansaction_task = PythonOperator(
+            task_id='extract_teansaction_task',
+            python_callable=pull_transaction_data
+        )
