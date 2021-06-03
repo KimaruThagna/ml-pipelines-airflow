282ab914c724173cd942504827c41cdca1ea7aea
Kimaru thagana <macbuntu@192.168.1.5>
2021-06-03 21:11:17 +0300

    add remote mock db to compose file

--- dags/ecommerce_etl_dag.py
+++ dags/ecommerce_etl_dag.py
@@ -1,12 +1,12 @@
+from datetime import datetime, timedelta
+
 from airflow import DAG
 from airflow.operators.http_operator import SimpleHttpOperator
 from airflow.operators.postgres_operator import PostgresOperator
-from airflow.operators.python import PythonOperator, task,
+from airflow.operators.python import PythonOperator
 from airflow.utils.task_group import TaskGroup
-
-from datetime import datetime, timedelta
-
 from ..etl_utils import *
+
 
 default_args = {
     "owner": "airflow",
@@ -21,40 +21,41 @@ default_args = {
 }
 
 
-with DAG("ECOMMERCE_ETL_DAG", default_args=default_args, schedule_interval=timedelta(days=1)) as dag: 
+with DAG(
+    "ECOMMERCE_ETL_DAG", default_args=default_args,
+    schedule_interval=timedelta(days=1)
+) as dag:
 
-    sample_base_filepath = ''
-    sample_remote_filepath = ''
+    sample_base_filepath = ""
+    sample_remote_filepath = ""
 
-    with TaskGroup("extraction_group") as extraction_group: 
+    with TaskGroup("extraction_group") as extraction_group:
         extract_users_task = PythonOperator(
-            task_id='extract_users_task',
-            python_callable=pull_user_data
+            task_id="extract_users_task", python_callable=pull_user_data
         )
-        
+
         extract_products_task = PythonOperator(
-            task_id='extract_products_task',
-            python_callable=pull_product_data
+            task_id="extract_products_task", python_callable=pull_product_data
         )
-         
+
         extract_teansaction_task = PythonOperator(
-            task_id='extract_teansaction_task',
-            python_callable=pull_transaction_data
-        )
-        
-        #demonstrate HttpOperator. In practice pulling data with lots of records may require
-        #SFTPOperator or PythonOperator that can pull process and store data.
+            task_id="extract_teansaction_task",
+            python_callable=pull_transaction_data)
+
+        # demonstrate HttpOperator. In practice pulling data with lots of records may require
+        # SFTPOperator or PythonOperator that can pull process and store data.
         demo_get_from_api = SimpleHttpOperator(
-        task_id="demo_get_from_api",
-        http_conn_id="mock-data-server-connection", # the connection will be set in Airflow Ui
-        endpoint="/users", 
-        method="GET",
-        xcom_push=True,
-        headers={"Content-Type": "application/json"},
-        log_response=True
-    )
+            task_id="demo_get_from_api",
+            # the connection will be set in Airflow Ui
+            http_conn_id="mock-data-server-connection",
+            endpoint="/users",
+            method="GET",
+            xcom_push=True,
+            headers={"Content-Type": "application/json"},
+            log_response=True,
+        )
         # sample SFTP for file download
-        
+
         # download_file = SFTPOperator(
         #         task_id=f"download_file",
         #         ssh_conn_id="file_server",
@@ -62,37 +63,34 @@ with DAG("ECOMMERCE_ETL_DAG", default_args=default_args, schedule_interval=timed
         #         remote_filepath=f"{sample_base_filepath}/filename",
         #         operation=SFTPOperation.GET,
         #         create_intermediate_dirs=True,
-                
+
         #         )
-        
+
     # processing/transformation tasks
     with TaskGroup("processing_group") as processing_group:
         create_table_platinum_customer_table = PostgresOperator(
-                    task_id='create_table_platinum_customer_table',
-                    sql=create_table_query,
-                        )
-        
+            task_id="create_table_platinum_customer_table", sql=create_table_query, )
+
         demo_xcom_pull = PythonOperator(
-            task_id='demo_xcom_pull',
-            python_callable=demonstrate_xcom_pull
-        ) 
-        
+            task_id="demo_xcom_pull", python_callable=demonstrate_xcom_pull
+        )
+
         generate_basket_analysis_csv_task = PythonOperator(
-            task_id='generate_basket_analysis_csv_task',
-            python_callable=get_basket_analysis_dataset
+            task_id="generate_basket_analysis_csv_task",
+            python_callable=get_basket_analysis_dataset,
         )
-        
+
         generate_recommendation_engine_csv_task = PythonOperator(
-            task_id='generate_recommendation_engine_csv_task',
-            python_callable=get_recommendation_engine_dataset
+            task_id="generate_recommendation_engine_csv_task",
+            python_callable=get_recommendation_engine_dataset,
         )
-        
+
         get_platinum_customer_task = PythonOperator(
-            task_id='get_platinum_customer_task',
-            python_callable=get_platinum_customer
-        )
-        
-    # we may want to send an email with files after processing. This can be done via an EmailOperator
+            task_id="get_platinum_customer_task",
+            python_callable=get_platinum_customer)
+
+    # we may want to send an email with files after processing. This can be
+    # done via an EmailOperator
     '''
         send_sample_dataset_email = EmailOperator(
             task_id='send_sample_dataset_email',
@@ -101,7 +99,7 @@ with DAG("ECOMMERCE_ETL_DAG", default_args=default_args, schedule_interval=timed
             html_content=""" <h1>Ecomm Industries Daily Summary and error report for {{ ds }}</h1> """,
             files=[f'{sample_base_filepath}/summary_data.csv', f'{sample_base_filepath}/error_logs.csv'],
             )
-        
+
     '''
-    
+
 extraction_group >> processing_group
